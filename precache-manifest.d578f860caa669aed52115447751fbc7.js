3f232845061d65ddfedffbcb6f3b60ab7afe2c66
miwsyed <syed97mustafa@gmail.com>
2021-02-22 16:47:05 +0530

    Updates

---
+++ precache-manifest.d578f860caa669aed52115447751fbc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b2e37f499b370bdcbf031537175f971",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "7563030d75fa3e599328",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "41f6be601d215431c3ff",
+    "url": "/box-office/static/js/2.e6cc4b4e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office/static/js/2.e6cc4b4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7563030d75fa3e599328",
+    "url": "/box-office/static/js/main.2a58d255.chunk.js"
+  },
+  {
+    "revision": "c850b6c555a3d841251e",
+    "url": "/box-office/static/js/runtime-main.e4c5afaf.js"
+  }
+]);
